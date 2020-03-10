a60b09e5548b475a788a15c27e671576428048e5
jeroentiggelman <infos@photographicdesign.be>
2020-03-10 19:11:12 +0100

    Added hero and info section on index page

---
+++ src/components/home/Info.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'gatsby'
+import Title from '../globals/Title'
+
+export default function Info () {
+  return (
+    <section className="py-5">
+      <div className="container">
+      <Title title="our story"></Title>
+        <div className="row">
+          <div className="col-10 col-sm-8 mx-auto text-center">
+            <p className="lead text-muted mb-5">
+              Lorem gipsum dolor sit amet, consectetur adipisicing elit. Nihil minus beatae fuga culpa voluptate fugit. Dolor reprehenderit hic, nulla iure sit odit perferendis, repudiandae dolores quae illum placeat natus suscipit possimus tempora facilis. Minus voluptatum iusto laudantium et, magnam, laborum, consectetur, quasi quidem praesentium in ullam rerum dolor quo excepturi.
+            </p>
+            <Link to="/about">
+              <button className="btn text-uppercase btn-yellow">about page</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
