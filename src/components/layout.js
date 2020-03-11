170027cd76222fb17b3936d32299cfb98783e419
jeroentiggelman <infos@photographicdesign.be>
2020-03-11 11:44:40 +0100

    footer added

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,12 +11,14 @@ import PropTypes from "prop-types"
 import "./bootstrap.min.css"
 import "./layout.css"
 
-import { Navbar } from "./globals/navbar"
+import { Navbar } from "./globals/Navbar"
+import Footer from "./globals/Footer"
 
 const Layout = ({ children }) =>
 <>
   <Navbar />
   {children}
+  <Footer />
 </>
 
 Layout.propTypes = {
