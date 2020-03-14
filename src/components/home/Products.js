9a1e810791c50b759c952d47ad9450e4b51b0b8f
jeroentiggelman <infos@photographicdesign.be>
2020-03-14 17:44:14 +0100

    correction graphql in products.js

--- src/components/home/Products.js
+++ src/components/home/Products.js
@@ -14,7 +14,7 @@ const getProducts = graphql`
           image {
             fluid(maxHeight: 426) {
               src
-              ...GatsbyContentfulFluid_tracedSVG
+              ...GatsbyContentfulFluid
             }
           }
         }
